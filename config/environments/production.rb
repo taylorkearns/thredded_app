fa01104b78270788d3789e2ac54c09fcdf00e706
Joel Oliveira <joel@thoughtbot.com>
2012-07-10 20:13:31 -0400

    Configure email in production
    * Add Sendgrid to config.
    * Add default url host in config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,6 +11,7 @@ Thredded::Application.configure do
   config.assets.digest = true
   config.assets.js_compressor  = :uglifier
 
+  config.action_mailer.default_url_options = { :host => 'www.thredded.com' }
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
 end
