d93f2bfc529f82760e2519cc06ecfc9185a9a835
Joel Oliveira <joel.oliveira@gmail.com>
2013-10-24 00:17:01 -0400

    [JRO] Update thredded gem and configure user_path

--- config/initializers/thredded.rb
+++ config/initializers/thredded.rb
@@ -2,3 +2,4 @@ Thredded.user_class = 'User'
 Thredded.email_incoming_host = 'incoming.thredded.com'
 Thredded.email_from = 'no-reply@thredded.com'
 Thredded.email_outgoing_prefix = '[Thredded Mail-Bot] '
+Thredded.user_path = ->(user){ "/users/#{user}" }
