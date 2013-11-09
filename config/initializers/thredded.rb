fd0b696434c3a3b0be95ba39a90f74b38e6c6601
Joel Oliveira <joel.oliveira@gmail.com>
2013-11-09 17:43:44 -0500

    [JRO] thredded 0.0.11 & config file storage

--- config/initializers/thredded.rb
+++ config/initializers/thredded.rb
@@ -3,3 +3,9 @@ Thredded.email_incoming_host = 'incoming.thredded.com'
 Thredded.email_from = 'no-reply@thredded.com'
 Thredded.email_outgoing_prefix = '[Thredded Mail-Bot] '
 Thredded.user_path = ->(user){ "/users/#{user}" }
+
+if Rails.env.production? || Rails.env.staging?
+  Thredded.file_storage = :fog
+else
+  Thredded.file_storage = :file
+end
