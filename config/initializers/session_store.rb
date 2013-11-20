9f2b4820f039d11dbb3a84626af8409a6a40a227
Joel Oliveira <joel.oliveira@gmail.com>
2013-11-20 12:25:11 -0500

    [JRO] Remove AR:SessionStore dependency & use own html-pipeline fork
    * AR session store is deprecated so switching to cache store on staging and production is desired
    * Using my html-pipeline fork is temporary until merged into upstream

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,9 +1,5 @@
-# Be sure to restart your server when you modify this file.
-
-# Thredded::Application.config.session_store :cookie_store, :key => '_thredded_session'
-Thredded::Application.config.session_store :active_record_store
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rake db:sessions:create")
-# Docs::Application.config.session_store :active_record_store
+if Rails.env.staging? || Rails.env.production?
+  Thredded::Application.config.session_store ActionDispatch::Session::CacheStore, expire_after: 1.day
+else
+  Thredded::Application.config.session_store :cookie_store, key: '_thredded_session'
+end
